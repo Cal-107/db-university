460aa792b5108db7b75a72d4d07e6b142205eb80
“Cal-107” <kaly___7@hotmail.it>
2022-01-14 14:05:00 +0100

    add GROUP BY exercises

--- db-uni.sql
+++ db-uni.sql
@@ -39,3 +39,24 @@ FROM departments;
 SELECT COUNT(id) as Teachers_without_phone
 FROM teachers
 WHERE `phone` IS NULL;
+
+----------------------------> GROUP BY <--------------------------------
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(`id`) as 'Count Students', YEAR(`enrolment_date`) AS 'Year enrolment'
+FROM `students` 
+GROUP BY YEAR(`enrolment_date`);
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(`id`) AS 'Teachers Count', `office_address` AS 'Building'
+FROM `teachers` 
+GROUP BY `office_address`;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT ROUND(AVG(`vote`), 1) AS 'Average Votes', `exam_id`
+FROM `exam_student` 
+GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(`id`) AS 'Degrees Count', `department_id`
+FROM `degrees` 
+GROUP BY `department_id`;
